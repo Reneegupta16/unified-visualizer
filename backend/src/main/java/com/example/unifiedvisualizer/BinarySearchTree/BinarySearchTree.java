c86ecaf461505a86f0d023be4126e1417c4955e4
Reneegupta16 <154832697+Reneegupta16@users.noreply.github.com>
2024-06-26 20:01:18 +0530

    Create BinarySearchTree.java

---
+++ backend/src/main/java/com/example/unifiedvisualizer/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,86 @@
+package com.example.unifiedvisualizer.bst;
+
+public class BinarySearchTree {
+    private Node root;
+
+    static class Node {
+        int key;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    private Node insertRec(Node root, int key) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+
+        return root;
+    }
+
+    public void deleteKey(int key) {
+        root = deleteRec(root, key);
+    }
+
+    private Node deleteRec(Node root, int key) {
+        if (root == null) return root;
+
+        if (key < root.key)
+            root.left = deleteRec(root.left, key);
+        else if (key > root.key)
+            root.right = deleteRec(root.right, key);
+        else {
+            if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+
+            root.key = minValue(root.right);
+            root.right = deleteRec(root.right, root.key);
+        }
+
+        return root;
+    }
+
+    private int minValue(Node root) {
+        int minv = root.key;
+        while (root.left != null) {
+            minv = root.left.key;
+            root = root.left;
+        }
+        return minv;
+    }
+
+    public boolean search(int key) {
+        return searchRec(root, key) != null;
+    }
+
+    private Node searchRec(Node root, int key) {
+        if (root == null || root.key == key)
+            return root;
+
+        if (root.key > key)
+            return searchRec(root.left, key);
+
+        return searchRec(root.right, key);
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+}
