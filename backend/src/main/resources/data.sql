74361ac3f2e9ef227b412926156dc034a589d9c3
Reneegupta16 <154832697+Reneegupta16@users.noreply.github.com>
2024-06-26 19:48:05 +0530

    Update data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,1 +1,5 @@
+CREATE DATABASE visualizer_db;
+CREATE USER 'visualizer_user'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON visualizer_db.* TO 'visualizer_user'@'localhost';
+FLUSH PRIVILEGES;
 
